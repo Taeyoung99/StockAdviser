063c69a90cde1bcab1c1aa5f59f630912103a95d
Taeyoung99 <67815439+Taeyoung99@users.noreply.github.com>
2021-09-10 23:50:00 -0400

    Add files via upload

---
+++ Stock Adviser.py
@@ -0,0 +1,6 @@
+#Receive the API from tradingview for stocks
+
+#Find the stocks that matches the criteria in basic financial statements(refer to the intelligent
+#investor and other sources)
+
+#Use the webscraping to get the relevant news regarding the stocks that u got on previous step.

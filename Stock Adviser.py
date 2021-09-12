f3f952f0f2b817ceb41aee740b59504afaf26f4a
Taeyoung99 <67815439+Taeyoung99@users.noreply.github.com>
2021-09-11 23:34:19 -0400

    Add files via upload

--- Stock Adviser.py
+++ Stock Adviser.py
@@ -1,6 +1,42 @@
 #Receive the API from tradingview for stocks
+import requests
+import json
+
+r = requests.get('https://www.tradingview.com/symbols/NASDAQ-SNDL/')
+data = json.loads(r)
+print(data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #Find the stocks that matches the criteria in basic financial statements(refer to the intelligent
 #investor and other sources)
 
+
+
+
+
+
+
+
+
+
+
+
 #Use the webscraping to get the relevant news regarding the stocks that u got on previous step.
+
+

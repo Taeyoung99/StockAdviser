cdddae9a797042de363907d46b7000f159249d2c
Taeyoung99 <67815439+Taeyoung99@users.noreply.github.com>
2021-10-20 15:58:10 -0400

    Add files via upload

---
+++ Stock Adviser database.sql
@@ -0,0 +1,8 @@
+CREATE TABLE companies(
+    Ticker varchar(100),
+    Market_cap varchar(100),
+    52_week_high varchar(100),
+    52_week_low varchar(100),
+    Price float
+    PRIMARY KEY(Ticker)
+);

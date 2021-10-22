779b651b1b09109a342e0acfecfbb74222492d89
Taeyoung99 <67815439+Taeyoung99@users.noreply.github.com>
2021-10-22 01:19:35 -0400

    Update Stockapi.py

--- Stockapi.py
+++ Stockapi.py
@@ -3,15 +3,34 @@ from alpha_vantage.timeseries import TimeSeries
 import time
 
 api_key = 'ICUTXZ1NUQTWDMNJ'
+topgainer = ["DWACU", "DWAC", "SGAMU", "TSC", "EAR"]
+
+
 
 ts = TimeSeries(key=api_key, output_format='pandas')
-data, meta_data = ts.get_intraday(symbol='MSFT', interval = '1min', outputsize = 'full')
+data, meta_data = ts.get_daily(symbol="DWACU", outputsize = 'full')
 print(data)
+
+ts = TimeSeries(key=api_key, output_format='pandas')
+data, meta_data = ts.get_daily(symbol="DWAC", outputsize = 'full')
+print(data)
+
+ts = TimeSeries(key=api_key, output_format='pandas')
+data, meta_data = ts.get_daily(symbol="SGAMU", outputsize = 'full')
+print(data)
+
+ts = TimeSeries(key=api_key, output_format='pandas')
+data, meta_data = ts.get_daily(symbol="TSC", outputsize = 'full')
+print(data)
+
+ts = TimeSeries(key=api_key, output_format='pandas')
+data, meta_data = ts.get_daily(symbol="EAR", outputsize = 'full')
+print(data)
+
 
 #make a loop function that runs through the list of top change in percentage stocks that day and evaluate if it has potential
 #Use technical analysis
 
-topgainer = ["DWACU", "DWAC", "SGAMU", "TSC", "EAR"]
 
 
          
